92e037e16359caf391afb7ce2eb982a333777d52
Hsu Lae Waddy <suwaddy9@gmail.com>
2026-01-18 19:41:36 +0630

    updated1

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import Slider from "../sections/Slider";
-import Header from "../sections/Header";
+import Footer from "../sections/Footer";
+import Header from "../sections/header";
 export default function Home() {
   return (
     <>
@@ -7,6 +8,7 @@ export default function Home() {
     <div className="bg-gray-400"> 
     <h2 className="text-4xl font-bold font-mono p-6 text-center">You can't hide who you are</h2>
     <Slider/></div>
+    <Footer></Footer>
     </>
   ); 
 }
